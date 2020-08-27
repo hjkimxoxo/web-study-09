7366b90384ad9d4d615d019cf91b34b4cfadaab6
hjkim <pjt911219@gmail.com>
2020-08-27 12:26:52 +0900

    디비 작업 완료

---
+++ test/web_study_09/ds/JdbcUtilTest.java
@@ -0,0 +1,20 @@
+package web_study_09.ds;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JdbcUtilTest {
+	
+
+	@Test
+	public void testGetConnection() {
+		Connection con = JdbcUtil.getConnection();
+		Assert.assertNotNull(con);
+		System.out.println(con);
+	}
+
+}
